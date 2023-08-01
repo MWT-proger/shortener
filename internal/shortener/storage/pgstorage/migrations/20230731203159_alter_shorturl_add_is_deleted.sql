ab3f8eb5b54a30bdbf13c3f4ba20c5411c1c15ea
MWTech <dry.wats@gmail.com>
2023-08-01 21:01:39 +0300

    поправил миграцию

--- internal/shortener/storage/pgstorage/migrations/20230731203159_alter_shorturl_add_is_deleted.sql
+++ internal/shortener/storage/pgstorage/migrations/20230731203159_alter_shorturl_add_is_deleted.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE "content"."shorturl" ADD COLUMN "is_deleted" bool;
+ALTER TABLE "content"."shorturl" ADD COLUMN "is_deleted" bool NOT NULL DEFAULT 'FALSE';
 -- +goose StatementEnd
 
 -- +goose Down
